ae8ffd2d4fab4dea07e84ce76c9639c09d4520d9
Michal Safranek <ms@dobris.net>
2023-08-21 10:53:50 +0200

    Web

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -14,130 +14,14 @@ use Mpdf\Mpdf;
 
 class MenuController extends Controller
 {
-    public function index(Request $r) {
-        return view('menuAdmin.select');
-    }
-
-    public function manage(Request $r, $id) {
-        $restaurant = Restaurant::where(['key' => $id])->first();
-        return view('menuAdmin.list', compact('restaurant'));
-    }
-
-    public function emails(Request $r, $id) {
-        $restaurant = Restaurant::where(['key' => $id])->first();
-        return view('menuAdmin.emails', compact('restaurant'));
-    }
-
-    public function getMealHistory(Request $r, $id) {
-        return MealHistory::where(['restaurantId' => $id])->get();
-    }
-
-    public function getMenus(Request $r, $id) {
-
-        $ret = ['menus' => []];
-        $date = new \DateTime();
-
-        // První datum 3 týdny zpět
-        $date->modify('-3 weeks');
-        $this->setMonday($date);
-
-        // W: Week number, N: Day in week (1-7)
 
 
-        for($i=0; $i<6; $i++) {
-            $dateSunday = $this->getSunday($date);
-            //echo $date->format("Y-W").": ".$date->format('j.n.Y')."-".$dateSunday->format('j.n.Y')."\n";
-
-            $menu = $this->getMenuForDate($date, $id);
-            $ret['menus'][] = $menu;
-
-            $date->modify('+1 week');
-        }
-
-        return $ret;
-    }
-
-    public function getEmails(Request $r, $id) {
-        return MenuEmail::where(['restaurant' => $id])->get();
-    }
-
-    public function addEmail(Request $r, $id) {
-        $validated = $r->validate([
-            'email' => 'required|email',
-        ]);
-        $e = new MenuEmail();
-        $e->restaurant = $id;
-        $e->email = $validated['email'];
-        $e->saveOrFail();
-        return MenuEmail::where(['restaurant' => $id])->get();
-    }
-
-    public function deleteEmail(Request $r, $restaurant, $id) {
-        $e = MenuEmail::where(['restaurant' => $restaurant, 'id' => $id])->delete();
-        return MenuEmail::where(['restaurant' => $restaurant])->get();
-    }
 
     private function setMonday(\DateTime &$date) {
         if($date->format('N') > 1) {
             $date->modify('-'.($date->format('N')-1).' days');
         }
     }
-
-    public function saveMenu(Request $r, $restaurant) {
-        $menu = $r->menu;
-        MenuMeal::where([
-            'restaurant' => $restaurant,
-            'year' => $menu['year'],
-            'week' => $menu['week']
-        ])->delete();
-
-        foreach($r->menu['days'] as $dayIndex => $day) {
-            echo $dayIndex;
-            foreach($day['meals'] as $mealIndex => $meal) {
-                $m = new MenuMeal(['restaurant' => $restaurant, 'year' => $menu['year'], 'week' => $menu['week']]);
-                $m->day = $dayIndex;
-                $m->mealId = $mealIndex;
-                $m->type = $meal['type'];
-                $m->ammount = $meal['ammount'];
-                $m->name = $meal['name']? $meal['name'] : '';
-                $m->price = $meal['price'] ? $meal['price'] : '0.00';
-                $m->alergens = $meal['alergens'] ? $meal['alergens'] : '';
-                $m->save();
-            }
-        }
-
-    }
-
-
-    public function printPdf(Request $request, $restaurant, $year, $week) {
-        $menu = Menu::where(['restaurant' => $restaurant, 'year' => $year, 'week' => $week])->first();
-        $mpdf = $menu->getPdf();
-        $mpdf->Output('menu.pdf','I');
-
-    }
-
-    public function emailPdf(Request $r, $restaurant, $year, $week) {
-        $menu = Menu::where(['restaurant' => $restaurant, 'year' => $year, 'week' => $week])->first();
-        $emails = MenuEmail::where(['restaurant' => $restaurant])->pluck('email');
-        $restaurant = Restaurant::where(['id' => $restaurant])->first();
-        return view('menuAdmin.sendEmails', compact('restaurant', 'menu', 'emails'));
-    }
-
-    public function sendEmail(Request $request) {
-        $validated = $request->validate([
-            'restaurant' => 'integer|required',
-            'year' => 'digits:4|required',
-            'week' => 'numeric|min:0|max:54|required',
-            'emails' => 'array',
-            'toEmail' => 'email|required'
-        ]);
-
-
-        $menu = Menu::where(['restaurant' => $validated['restaurant'], 'year' => $validated['year'], 'week' => $validated['week']])->first();
-        Mail::send(new SendMenu($menu, $validated['toEmail'], $validated['emails']));
-    }
-
-
 
 
 
--- routes/web.php
+++ routes/web.php
@@ -17,30 +17,4 @@ Route::get('/', function () {
     return view('dailyMenu');
 });
 
-
-Route::get('/menu', [\App\Http\Controllers\MenuController::class, 'index'])->middleware('auth');
-Route::get('/menu/{id}', [\App\Http\Controllers\MenuController::class, 'manage'])->where('id', 'zamek|kocka')->middleware('auth');
-Route::get('/menu/mealHistory/{id}', [\App\Http\Controllers\MenuController::class, 'getMealHistory'])->middleware('auth');
-Route::get('/menu/getMenus/{id}' ,[\App\Http\Controllers\MenuController::class, 'getMenus'])->middleware('auth');
-Route::get('/menu/print/{restaurant}/{year}/{week}', [\App\Http\Controllers\MenuController::class, 'printPdf'])->middleware('auth');
-Route::get('/menu/email/{restaurant}/{year}/{week}', [\App\Http\Controllers\MenuController::class, 'emailPdf'])->middleware('auth');
-Route::post('/menu/send', [\App\Http\Controllers\MenuController::class, 'sendEmail'])->middleware('auth');
-Route::post('/menu/{restaurant}', [\App\Http\Controllers\MenuController::class, 'saveMenu'])->middleware('auth');
-Route::get('/todayMenu', [\App\Http\Controllers\MenuController::class, 'todayMenu']);
-
-
-
-
-
-Route::get('/menu/emails/{id}', [\App\Http\Controllers\MenuController::class, 'emails'])->where('id', 'zamek|kocka')->middleware('auth');
-Route::get('/menu/getEmails/{id}', [\App\Http\Controllers\MenuController::class, 'getEmails'])->middleware('auth');
-Route::post('/menu/emails/{id}/add', [\App\Http\Controllers\MenuController::class, 'addEmail'])->middleware('auth');
-Route::delete('/menu/emails/{restaurant}/{id}', [\App\Http\Controllers\MenuController::class, 'deleteEmail'])->middleware('auth');
-
-Auth::routes([
-    'register' => false,
-    'reset' => false,
-    'verify' => false
-]);
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
